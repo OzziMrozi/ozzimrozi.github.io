9619fd07d51b8ac32015093424fdb9394b2b8b5d
OzziMrozi <oskosadnik@gmail.com>
2022-10-17 17:38:02 +0200

    fdgg

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-prompt("Jak ci mija dzień ? :)))");
-alert("Spoko nie pytałem zabij sie zjebie matka cie nie kocha stfu");
+var odp = prompt("Czy jesteś gotowy na niesamowite doznania wchodząt na tą stronę ? tak/nie");
+
+if(odp == "tak"){
+    alert("no i zajebiście");
+}else if(odp == "nie"){
+    alert("to wypierdalaj es");
+}else{
+    alert("tak albo nie, co jest w tym trudnego ?");
+}
+
